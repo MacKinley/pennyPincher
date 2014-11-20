f564a0ac91e6b6d366a83fe60e07132b7c81b043
MacKinley Trudeau <mackinley@fluidmarkup.com>
2014-11-19 21:13:29 -0500

    fixed scraper test

--- test/scraper.js
+++ test/scraper.js
@@ -4,20 +4,20 @@ var should = require('chai').should(),
 describe('#scrape', function() {
     // scrape static file to test if the actual data scraped remains consistent
     it('scrape local file', function() {
-        scrape('file:///Users/MacK/Desktop/Untitled.html', function(json) {
-            json.success.should.equal(true);
-            json.price.should.equal(14.99);
-            json.title.should.equal("SanDisk Cruzer 32GB USB 2.0 Flash Drive, Frustration-Free Packaging- SDCZ36-032G-AFFP");
+        scrape('file:///Users/MacK/Desktop/Untitled.html', function(response) {
+            response.success.should.equal(true);
+            response.price.should.equal(14.99);
+            response.title.should.equal("SanDisk Cruzer 32GB USB 2.0 Flash Drive, Frustration-Free Packaging- SDCZ36-032G-AFFP");
         });
     });
     
     // scrape live page to test if we are still able to scrape title and price
     it('scrape live hosted product', function() {
         scrape('http://amzn.com/B0011URFRE', function(response) {
-            json.success.should.equal(true);
-            json.price.should.be.a('double');
-            json.title.should.not.equal("");
-            json.title.should.be.a('string');
+            response.success.should.equal(true);
+            response.price.should.be.a('double');
+            response.title.should.not.equal("");
+            response.title.should.be.a('string');
         });
     });
 });
