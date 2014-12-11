db195e995ff64f0ba5a1f794051f0d0168aa44c1
MacKinley Trudeau <mackinley@fluidmarkup.com>
2014-12-11 17:11:46 -0500

    fixed try catch inside scraper.js, fixed unit tests, and removed test.html to a seperate server

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -3,19 +3,8 @@ var fs      = require('fs'),
     cheerio = require('cheerio');
 
 module.exports = {
-    // attempt to scrape the url
     scrape: function (url, callback){
-        try{
-            scrapeHelper(url, function(response){
-                callback(false, response);
-            });
-        } catch(err) {
-            callback(err, {});
-        }
-    }
-};
 
-function scrapeHelper(url, callback){
     // TODO call json factory to create this
     var productJson = {
         success : false,
@@ -25,85 +14,94 @@ function scrapeHelper(url, callback){
         desc    : [],
         brand   : "",
         inStock : "",
-        url     : "" 
+        url     : ""
     };
-    
+
     productJson.url = url;
+    request(url, function(err, response, html){
+        if(!err){
+            try{
+                var $ = cheerio.load(html);
 
-    request(url, function(error, response, html){
-        if(!error){
-            var $ = cheerio.load(html);
-            
-            // get title
-            $('#btAsinTitle').filter(function(){
-                productJson.title = $(this).text();
-            })
-            
-            // get price
-            $('#actualPriceValue').filter(function(){
-                var priceDOM = $(this).find('b');
-                // get rid of '$'
-                productJson.price = parseFloat(priceDOM.text().substring(1));
-            })
-            
-            // try in the event that the product has not been rating yet
-            try {
-                // get avg rating
-                $('.crAvgStars').first().filter(function(){
-                    var custReviewsSpan = $(this);
-                    var reviewStarsTitle = custReviewsSpan
-                            .children().first()
-                            .children().first()
-                            .children().first()
-                            .attr('title');
-                    
-                    // get number of stars from title
-                    productJson.rating = parseFloat(
-                            reviewStarsTitle.substring(0,
-                                reviewStarsTitle.indexOf(' ')));
+                // get title
+                $('#btAsinTitle').filter(function(){
+                    productJson.title = $(this).text();
                 })
-            } catch(err) {
-                // if product has never been rating
-                productJson.rating = -1;
+
+                // get price
+                $('#actualPriceValue').filter(function(){
+                    var priceDOM = $(this).find('b');
+                    // get rid of '$'
+                    productJson.price = parseFloat(priceDOM.text().substring(1));
+                })
+
+                // try in the event that the product has not been rating yet
+                try {
+                    // get avg rating
+                    $('.crAvgStars').first().filter(function(){
+                        var custReviewsSpan = $(this);
+                        var reviewStarsTitle = custReviewsSpan
+                                .children().first()
+                                .children().first()
+                                .children().first()
+                                .attr('title');
+
+                        // get number of stars from title
+                        productJson.rating = parseFloat(
+                                reviewStarsTitle.substring(0,
+                                    reviewStarsTitle.indexOf(' ')));
+                    })
+                } catch(err) {
+                    // if product has never been rating
+                    productJson.rating = -1;
+                }
+
+                // get description
+                $('#feature-bullets-atf').filter(function(){
+                    var bulletText, desc = [];
+                    var bullets = $(this).children().first()
+                            .children().first()
+                            .children().first()
+                            .children().first()
+                            .children().first()
+                            .children();
+
+                    // get text from each bullet
+                    bullets.each(function(i){
+                        bulletText = $(this).children().first().text();
+                        desc = desc.concat(bulletText);
+                    })
+
+                    productJson.desc = desc;
+                })
+
+                // get brand
+                $('#product-title_feature_div').filter(function(){
+                    brand = $(this).children().first()
+                            .find('span').children().first().text();
+                    productJson.brand = brand;
+                })
+
+                // get stock status
+                $('#availability_feature_div').filter(function(){
+                    // if class is 'availGreen' its in stock
+                    inStock = ($(this).children().first().children()
+                                .first().attr('class') == 'availGreen');
+                    productJson.inStock = inStock;
+                })
+
+                // if a title and price were scraped consider it a success
+                if(productJson.title != "" && productJson.price != "")
+                    productJson.success = true;
+
+                callback(null, productJson);
+            }catch(err){
+                callback(err, null);
             }
-
-            // get description
-            $('#feature-bullets-atf').filter(function(){
-                var bulletText, desc = [];
-                var bullets = $(this).children().first()
-                        .children().first()
-                        .children().first()
-                        .children().first()
-                        .children().first()
-                        .children();
-
-                // get text from each bullet
-                bullets.each(function(i){
-                    bulletText = $(this).children().first().text();
-                    desc = desc.concat(bulletText);
-                })
-                
-                productJson.desc = desc;
-            })
-
-            // get brand
-            $('#product-title_feature_div').filter(function(){
-                brand = $(this).children().first().find('span').children().first().text();
-                productJson.brand = brand;
-            })
-            
-            // get stock status
-            $('#availability_feature_div').filter(function(){
-                // if class is 'availGreen' its in stock
-                inStock = ($(this).children().first().children().first().attr('class') == 'availGreen');
-                productJson.inStock = inStock;
-            })
-
-            // if a title and price were scraped consider it a success
-            if(productJson.title != "" && productJson.price != "")
-                productJson.success = true;
-
-            callback(productJson);
+        }else{
+            callback(err, null);
         }
-    })
-}
+    });
+    }
+};
+
--- scraper/test/scraper.js
+++ scraper/test/scraper.js
@@ -1,33 +1,45 @@
 var should = require('chai').should(),
     scrape = require('../scraper.js').scrape;
 
-describe('#scrape', function() {
-    // scrape static file to test if the actual data scraped remains consistent
-    it('scrape local file', function() {
-        scrape('file:./test.html', function(err, response) {
+describe('scraper', function() {
+    var staticProduct, liveProduct;
+    this.timeout(10000);
+
+    before(function(done){
+        // scrape a live known product
+        scrape('http://amzn.com/B0011URFRE', function(err, productResponse){
             if(!err){
-                response.success.should.equal(true);
-                response.price.should.equal(14.99);
-                response.title.should.equal("SanDisk Cruzer 32GB USB 2.0 Flash Drive, Frustration-Free Packaging- SDCZ36-032G-AFFP");
-            }else{
-                // ensure the test fails
-                err.should.equal(false);
+                liveProduct = productResponse;
+            } else {
+                throw(err);
             }
+
+            // scrape a static (not hosted by amazon) product
+            scrape('http://www.cis.umassd.edu/~mtrudeau/pp/test.html',
+                    function(err, productResponse){
+                if(!err){
+                    staticProduct = productResponse;
+                } else {
+                    throw(err);
+                }
+                done();
+            });
         });
     });
-    
-    // scrape live page to test if we are still able to scrape title and price
+
+    // if this fails then we broke the scraper
+    it('scrape local file', function() {
+        staticProduct.success.should.equal(true);
+        staticProduct.price.should.equal(14.99);
+        staticProduct.title.should.equal("SanDisk Cruzer 32GB USB 2.0 Flash Drive, Frustration-Free Packaging- SDCZ36-032G-AFFP");
+    });
+
+    // if this fails then the html has changed or product has been removed
     it('scrape live hosted product', function() {
-        scrape('http://amzn.com/B0011URFRE', function(err, response) {
-            if(!err){
-                response.success.should.equal(true);
-                response.price.should.be.a('double');
-                response.title.should.not.equal("");
-                response.title.should.be.a('string');
-            }else{
-                // ensure the test fails
-                err.should.equal(false);
-            }
-        });
+        liveProduct.success.should.equal(true);
+        liveProduct.price.should.be.a('number');
+        liveProduct.title.should.not.equal("");
+        liveProduct.title.should.be.a('string');
     });
 });
+
