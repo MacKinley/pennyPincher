7e93c44668dc6963a1dfcbc5c9c4b50484702606
Marvin <mcelestin7@gmail.com>
2015-02-18 14:20:31 -0500

    created prototype emailer (not working)

---
+++ emailer/emailer.js
@@ -0,0 +1,53 @@
+var mailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+//module.exports = mail(){
+
+	//mailUser : function(product, user, callback) {
+		//set up mail message
+
+		// Sets up the email transportation
+		var transport = mailer.createTransport(
+		smtpTransport({
+			service: "gmail",
+			auth: {
+
+				user: 'ppincher9@gamil.com',
+				pass: 'pennypincher9'
+
+				}
+		}));
+
+		var mailOption = {
+
+			from: "ppincher9@gamil.com",
+			to: "mcelestin7@gmail.com",
+			Subject: "Penny Pinchers Update on Subscription",
+			text: "Check out the new price on this item "
+
+		};
+
+		console.log('Sending Mail');
+
+		transport.sendMail(mailOption, function(error, response){
+			/*if(callback != null){
+				callback(err, )
+			}*/
+
+			if(error){
+				console.log(error);
+
+			}
+			else{
+				console.log("message sent" + response.message);
+			}
+		});
+		
+	//};
+//}
+
+
+
+
+
+
+
