5cfeabfdff3513c3559ba60522488da1759f116b
Marvin <mcelestin7@gmail.com>
2015-02-19 15:24:33 -0500

    Working Prototype now a functional module

--- emailer/emailer.js
+++ emailer/emailer.js
@@ -1,54 +1,47 @@
 var mailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
-//module.exports = mail(){
 
-	//mailUser : function(product, user, callback) {
-		//set up mail message
+// Sets up the email transportation
+var transport = mailer.createTransport(
+	smtpTransport({
+		service: "gmail",
+		auth: {
+			user: 'ppincher9',
+			pass: 'pennypincher9'
+		}
+	})
+);
 
-		// Sets up the email transportation
-		var transport = mailer.createTransport(
-		smtpTransport({
-			service: "gmail",
-			auth: {
+var mailOption;
 
-				user: 'ppincher9',
-				pass: 'pennypincher9'
+module.exports = {
+	//console.log('Sending Mail');
 
-				}
-		}));
+	sendMail : function(product, users, callback){
 
-		var mailOption = {
+		//
+		createMailOption(prodcut, user, function(mailOpt){
+			this.mailOption = mailOpt;
+		});
 
-			from: "Penny Pinchers",
-			to: 'Recipient',
-			bcc:'"mcelestin7@gmail.com","mtrudeau@umassd.edu","vwh1987@hotmail.com","eefahey@hotmail.com"',
-			subject: "Penny Pinchers Update on Subscription",
-			text: "Check out the new price on this item, also this is a test "
-
-		};
-
-		console.log('Sending Mail');
-
-		transport.sendMail(mailOption, function(error, response){
-			/*if(callback != null){
-				callback(err, )
-			}*/
-
-			if(error){
-				console.log(error);
-
-			}
-			else{
-				console.log("message sent" + response.message);
+		transport.sendMail(mailOption, function(err, response){
+			if(callback != null){
+				callback(err, response)
 			}
 		});
-		
-	//};
-//}
+	}
+};
 
+function createMailOption(product, users, callback) {
+	var mailOpt = {
 
+		from: "Penny Pinchers",
+		to: 'Recipient',
+		bcc:'"mcelestin7@gmail.com","mtrudeau@umassd.edu","vwh1987@hotmail.com","eefahey@hotmail.com" zmori',
+		subject: "Penny Pinchers Update on Subscription",
+		text: "Check out the new price on this item,"+product.title+"now"+product.price+"also this is a test "
 
+	};
 
-
-
-
+	callback(mailOpt);
+}
