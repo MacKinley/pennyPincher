9c5d6dad4eb0c6db831e503fe8f2a373ceee2620
MacKinley Trudeau <mackinley@fluidmarkup.com>
2015-02-25 17:03:43 -0500

    minor spacing

--- public/app.js
+++ public/app.js
@@ -2,8 +2,9 @@ angular.module('app', ['ngRoute', 'productDetail'])
 .config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-		when('/product/:asin', {
-		  templateUrl: 'product-detail/product-detail-partial.html',
-		  controller: 'ProductDetail'
-		  });
+      when('/product/:asin', {
+        templateUrl: 'product-detail/product-detail-partial.html',
+        controller: 'ProductDetail'
+      });
   }]);
+
