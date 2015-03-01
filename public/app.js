eaf9547df9cad7bcb8e221cde0a11de619c494db
MacKinley Trudeau <mackinley@fluidmarkup.com>
2015-02-28 22:02:11 -0500

    created homepage partial and added route + minor css changes

--- public/app.js
+++ public/app.js
@@ -1,10 +1,14 @@
-angular.module('app', ['ngRoute', 'productDetail'])
+angular.module('app', ['ngRoute', 'homepage', 'productDetail'])
 .config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/product/:asin', {
-        templateUrl: 'product-detail/product-detail-partial.html',
+        templateUrl: './product-detail/product-detail-partial.html',
         controller: 'ProductDetail'
+      }).
+      otherwise({
+        templateUrl: './homepage/homepage-partial',
+        controller: 'Homepage'
       });
   }]);
 
---
+++ public/homepage/homepage.js
@@ -0,0 +1,3 @@
+angular.module('homepage', [])
+.controller('Homepage', []);
+
