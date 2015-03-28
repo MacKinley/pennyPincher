6c65bfef71d21459c44951d846349b4c8ff9f832
MacKinley Trudeau <mackinley@fluidmarkup.com>
2015-03-28 09:24:09 -0400

    setup angular to route without the #, created service to get product data from backend, and created service to share product data with graph controller

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 angular.module('app', ['ngRoute', 'ui.bootstrap',
     'homepage', 'loginPopup', 'productDetail'])
-.config(['$routeProvider',
-  function($routeProvider) {
+.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
     $routeProvider.
       when('/product/:asin', {
         templateUrl: './product-detail/product-detail-partial.html',
@@ -10,18 +10,18 @@ angular.module('app', ['ngRoute', 'ui.bootstrap',
       otherwise({
         templateUrl: './homepage/homepage-partial'
       });
+      $locationProvider.html5Mode(true);
 }])
 .constant('API_ENDPOINT', 'http://localhost:3000/')
-.service('ProductService', ['$http', 'API_ENDPOINT', function($http, apiEndpoint){
-  this.getProduct = function(asin, callback){
-    $http.get(apiEndpoint + 'product:' + asin)
-    .success(
-      function(data, status, headers, config){
-      callback(data);
-    }).error(
-      function(data, status, headers, config){
-      callback(data);
-    });
-  };
+.service('ProductService', ['$http', 'API_ENDPOINT',
+  function($http, apiEndpoint){
+    this.getProduct = function(asin, callback){
+      $http.get(apiEndpoint + 'product:' + asin)
+      .success(function(data, status, headers, config){
+        callback(data);
+      }).error(function(data, status, headers, config){
+        callback(data);
+      });
+    };
 }]);
 
--- public/product-detail/product-detail.js
+++ public/product-detail/product-detail.js
@@ -1,59 +1,65 @@
 angular.module('productDetail', ['n3-line-chart'])
-.controller('ProductDetail', ['$scope', '$routeParams', 'ProductService',
-  function($scope, $routeParams, ProductService) {
+.controller('ProductDetail',
+    ['$scope', '$routeParams', 'ProductService', 'PriceSharer',
+  function($scope, $routeParams, ProductService, PriceSharer) {
     ProductService.getProduct($routeParams.asin,
     function(response){
       $scope.product = response;
     });
   }
 ])
-.controller("Graph", function ($scope) {
-  $scope.data = [
-    {
-      x: new Date(1422072611079),
-      price: 0.00
-    },
-    {
-      x: new Date(1422159011079),
-      price: 8.47
-    },
-    {
-      x: new Date(1422245411079),
-      price: 13.98
-    },
-    {
-      x: new Date(1422331811079),
-      price: 14.60
-    },
-    {
-      x: new Date(1422431811079),
-      price: 15.23
-    }
-  ];
-  $scope.options = {
-    lineMode: "linear",
-    tension: 1,
-    axes: {x: {type: "date", key: "x", ticks: 5}, y: {type: "linear"}},
-    drawLegend: false,
-    series: [
+.service('PriceSharer', function(){
+  this.prices = [
       {
-        y: "price",
-        color: "#3A74C5",
-        axis: "y",
-        type: "line",
-        thickness: "3px",
-        drawDots: false
+        x: new Date(1422072611079),
+        price: 0.00
+      },
+      {
+        x: new Date(1422159011079),
+        price: 8.47
+      },
+      {
+        x: new Date(1422245411079),
+        price: 13.98
+      },
+      {
+        x: new Date(1422331811079),
+        price: 14.60
+      },
+      {
+        x: new Date(1422431811079),
+        price: 15.23
       }
-    ],
-    tooltip: {
-      mode: "scrubber",
-      formatter: function (x, y, series) {
-        return x.getMonth()+1
-            + '/' + x.getDate()
-            + '/' + x.getUTCFullYear() + ' : '
-            + '$' + y;
+  ];
+})
+.controller('Graph', ['$scope', 'PriceSharer',
+  function($scope, PriceSharer){
+    $scope.data = PriceSharer.prices;
+    $scope.options = {
+      lineMode: "linear",
+      tension: 1,
+      axes: {x: {type: "date", key: "x", ticks: 5}, y: {type: "linear"}},
+      drawLegend: false,
+      series: [
+        {
+          y: "price",
+          color: "#3A74C5",
+          axis: "y",
+          type: "line",
+          thickness: "3px",
+          drawDots: false
+        }
+      ],
+      tooltip: {
+        mode: "scrubber",
+        formatter: function (x, y, series) {
+          return x.getMonth()+1
+              + '/' + x.getDate()
+              + '/' + x.getUTCFullYear() + ' : '
+              + '$' + y;
+        }
       }
-    }
-  };
-});
+    };
+  }
+]);
 
