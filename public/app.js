28e27db27eafa62f4bd5d859a2f82c0b11562a58
Venus Ho <vho@umassd.edu>
2015-03-30 18:52:38 -0400

    user/:id for userOpts

--- public/app.js
+++ public/app.js
@@ -1,10 +1,17 @@
-angular.module('app', ['ngRoute', 'productDetail'])
+angular.module('app', ['ngRoute', 'ui.bootstrap',
+    'homepage', 'loginPopup', 'productDetail'])
 .config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/product/:asin', {
-        templateUrl: 'product-detail/product-detail-partial.html',
+        templateUrl: './product-detail/product-detail-partial.html',
         controller: 'ProductDetail'
+      }).
+	  when('/user/:userId', {
+        templateUrl: 'userOpts/userOpts-partial.html',
+        controller: 'UserOpts'
+      }).
+      otherwise({
+        templateUrl: './homepage/homepage-partial'
       });
   }]);
-
