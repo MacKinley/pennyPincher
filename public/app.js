be997dda0b7b65885c46e3a781bf2c6d807464e2
MacKinley Trudeau <mackinley@fluidmarkup.com>
2015-03-31 14:51:41 -0400

    moved Venus and Erin's userOpts partial from productPage branch to the branch and hardcoded some styles into the html

--- public/app.js
+++ public/app.js
@@ -7,8 +7,12 @@ angular.module('app', ['ngRoute', 'ui.bootstrap',
         templateUrl: './product-detail/product-detail-partial.html',
         controller: 'ProductDetail'
       }).
+      when('/user/:userId', {
+        templateUrl: './userOpts/userOpts-partial.html'
+      }).
       otherwise({
         templateUrl: './homepage/homepage-partial'
       });
-  }]);
+  }
+]);
 
